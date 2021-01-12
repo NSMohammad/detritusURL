559b0250c5f9c75ae48846c3cb48a04609e5268f
Mohamad Deghan <m.play.assassin@gmail.com>
2021-01-12 09:22:43 +0330

    Initial Commit

---
+++ ShortMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShortMe
+//
+//  Created by MuhaMaD on 10/23/1399 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShortMe/ShortMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortMeApp.swift
+//  ShortMe
+//
+//  Created by MuhaMaD on 10/23/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct ShortMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
